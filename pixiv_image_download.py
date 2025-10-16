bf84e66f3736b9b5fccc0a17bc55847ef75acffe
cshuihui <xzj200669@foxmail.com>
2025-10-16 22:25:38 +0800

    更新

--- pixiv_image_download.py
+++ pixiv_image_download.py
@@ -6,6 +6,8 @@ import os
 import random
 
 save_dir = 'download'
+R18_rem = 0
+R18G_rem = 0
 
 
 def link_to_image(sub_dir, image_name, pid_link):
@@ -33,7 +35,7 @@ def link_to_image(sub_dir, image_name, pid_link):
                  '-lOci6hh5jjnmGdeeUDtZ83WndbyRZtywdaDKC695p7NeoN_PwldsmZy5jHzQvixJ3AL0Qh6'
                  '-LDChj5UOmnQED8Uabak_FUQVkPUhfScRtxhYTmUlYI-w%3D%3D%22%5D%5D',
     }
-    for i in range(0, 3):
+    for i in range(1, 4):
 
         try:
             response = requests.get(pid_link, headers=headers, timeout=10, cookies=cookies)
@@ -69,16 +71,34 @@ def user_input():
             continue
         break
     while 1:
+        r18_rem = input('R18标签标记（0/1）:')
+        if r18_rem == '':
+            r18_rem = 0
+            break
+        elif int(r18_rem) != 1 and int(r18_rem) != 0:
+            print('标签格式错误！请重新输入')
+            continue
+        break
+    while 1:
+        r18g_rem = input('R18G标签是否标记（0/1）:')
+
+        if r18g_rem == '':
+            r18g_rem = 0
+            break
+        elif int(r18g_rem) != 0 and int(r18g_rem) != 1:
+            print('标签格式错误！请重新输入')
+            continue
+        break
+    while 1:
         start_page = input("输入起始页(默认为1):")
         if start_page == '':
             start_page = 1
-
+            break
         elif true_page(start_page) or int(start_page) == 0:
             print('起始页格式错误！请重新输入')
             time.sleep(1)
             continue
         break
-
     while 1:
         last_page = input("输入结束页(默认当前页):")
         if last_page == '':
@@ -95,11 +115,11 @@ def user_input():
             continue
         break
 
-    return search_content, int(start_page), int(last_page)
+    return search_content, int(r18_rem), int(r18g_rem), int(start_page), int(last_page)
     # 在 while 里定义的变量 start_page，它其实是整个函数 user_input() 的局部变量。
 
 
-def image_download(name, phpsessid, start_page=1, last_page=1):
+def image_download(name, phpsessid, r18_rem, r18g_rem, start_page=1, last_page=1):
     sub_dir = os.path.join(save_dir, name)  # download/nahida os会自动处理是 / 还是 \
     os.makedirs(save_dir, exist_ok=True)
     os.makedirs(sub_dir, exist_ok=True)
@@ -129,28 +149,32 @@ def image_download(name, phpsessid, start_page=1, last_page=1):
         print(f"正在下载第 {pages} 页的图片")
         time.sleep(random.randint(1, 3))
         for index, pid in enumerate(pid_list, start=1):
-            pid_link = pixiv_imagelink.link_find(phpsessid, pid)
-
-            image_name = pid_link.split(sep='/')[-1]
+            pid_links = pixiv_imagelink.link_find(phpsessid, pid)
+            if pid_links['R18'] != R18_rem or pid_links['R18G'] != R18G_rem:
+                print(pid, '已过滤')
+                continue
+            image_name = pid_links['link'].split(sep='/')[-1]
             print(f"正在下载 {image_name} ({index}/{len(pid_list)})")
-            for i in range(0, 10):
-                temp_name = image_name.replace(image_name.split('_')[1],
-                                               image_name.split('_')[1].replace('p0', 'p' + str(i)))
-                temp_link = pid_link.replace(image_name, temp_name)
+            for i in range(0, 15):
+                temp_list = list(image_name)
+                temp_list[image_name.find('.')-1] = str(i)
+                temp_name = ''.join(temp_list)  # 列表转字符串
+                print(temp_name)
+                temp_link = pid_links['link'].replace(image_name, temp_name)
                 if not link_to_image(sub_dir, temp_name, temp_link):
                     break
 
             print("下载完成！准备下载下一个…")
-            time.sleep(random.uniform(0.5, 1.5))    # uniform 用于小数点之间的随机数 输出为float类型
+            time.sleep(random.uniform(0.5, 1.5))  # uniform 用于小数点之间的随机数 输出为float类型
 
 
 if __name__ == "__main__":
     with open("phpsessid.txt", 'r') as f:
         phpsessid = f.read()
     while 1:
-        search_content, start_page, last_page = user_input()
+        search_content, R18_rem, R18G_rem, start_page, last_page = user_input()
 
-        image_download(search_content, phpsessid, start_page=start_page, last_page=last_page)
+        image_download(search_content, phpsessid, R18_rem, R18G_rem, start_page=start_page, last_page=last_page)
 
         while 1:
             choose = input('是否继续搜索？(Y/N)')
